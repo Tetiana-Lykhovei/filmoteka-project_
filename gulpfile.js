200c2a1ec4df112c2e63edafe613a245b9d83cbb
Tetiana-Lykhovei <tlykhovei@gmail.com>
2021-02-16 20:53:47 +0200

    i

--- gulpfile.js
+++ gulpfile.js
@@ -58,3 +58,10 @@ var deploy = require("gulp-gh-pages");
 gulp.task("deploy", function () {
   return gulp.src("./dist/**/*").pipe(deploy());
 });
+
+var gulp = require("gulp");
+var ghPages = require("gulp-gh-pages");
+
+gulp.task("deploy", function () {
+  return gulp.src("./dist/**/*").pipe(ghPages());
+});
